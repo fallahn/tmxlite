a8aaaf139961b3c9f0c74a410c5ae871abd49627
fallahn <matty_styles@hotmail.com>
2016-11-14 10:24:31 +0000

    add small program to test parsing files

---
+++ ParseTest/src/main.cpp
@@ -0,0 +1,74 @@
+/*********************************************************************
+Matt Marchant 2016
+http://trederia.blogspot.com
+
+tmxlite - Zlib license.
+
+This software is provided 'as-is', without any express or
+implied warranty. In no event will the authors be held
+liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute
+it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented;
+you must not claim that you wrote the original software.
+If you use this software in a product, an acknowledgment
+in the product documentation would be appreciated but
+is not required.
+
+2. Altered source versions must be plainly marked as such,
+and must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any
+source distribution.
+*********************************************************************/
+
+#include <tmxlite/Map.hpp>
+
+#include <iostream>
+
+int main()
+{
+    tmx::Map map;
+
+    if (map.load("maps/platform.tmx"))
+    {
+        std::cout << "Loaded Map version: " << map.getVersion().upper << ", " << map.getVersion().lower << std::endl;
+        
+        const auto& mapProperties = map.getProperties();
+        std::cout << "Map has " << mapProperties.size() << " properties" << std::endl;
+        for (const auto& prop : mapProperties)
+        {
+            std::cout << "Found property: " << prop.getName() << std::endl;
+            std::cout << "Type: " << int(prop.getType()) << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        const auto& layers = map.getLayers();
+        std::cout << "Map has " << layers.size() << " layers" <<  std::endl;
+        for (const auto& layer : layers)
+        {
+            std::cout << "Found Layer: " << layer->getName() << std::endl;
+            std::cout << "Layer Type: " << int(layer->getType()) << std::endl;
+
+            const auto& properties = layer->getProperties();
+            std::cout << properties.size() << " Layer Properties:" << std::endl;
+            for (const auto& prop : properties)
+            {
+                std::cout << "Found property: " << prop.getName() << std::endl;
+                std::cout << "Type: " << int(prop.getType()) << std::endl;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Failed loading map" << std::endl;
+    }
+
+    system("pause");
+
+    return 0;
+}
