88ba06a7a08ea3155478c9d3d0c110e0b17572da
fallahn <matty_Styles@hotmail.com>
2016-11-20 13:19:24 +0000

    fix typo in FindTMXLITE.cmake

--- OpenGLExample/cmake/modules/FindTMXLITE.cmake
+++ OpenGLExample/cmake/modules/FindTMXLITE.cmake
@@ -1,10 +1,10 @@
 include(FindPackageHandleStandardArgs)
 
 # Search for the header file
-find_path(TMXLITE_INCLUDE_DIR tmxlite/Config.hpp PATH_SUFFIXES include)
+find_path(TMXLITE_INCLUDE_DIR NAMES tmxlite/Config.hpp PATH_SUFFIXES include)
 
 # Search for the library
-find_library(TMXLITE_LIBRARIES NAMES libtmxlite PATH_SUFFIXES lib)
+find_library(TMXLITE_LIBRARIES NAMES tmxlite PATH_SUFFIXES lib)
 
 # Did we find everything we need?
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(libtmxlite DEFAULT_MSG TMXLITE_LIBRARIES TMXLITE_INCLUDE_DIR) 
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(tmxlite DEFAULT_MSG TMXLITE_LIBRARIES TMXLITE_INCLUDE_DIR) 
