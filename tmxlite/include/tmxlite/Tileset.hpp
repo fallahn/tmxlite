a6309e971ee3c82846e6bfe662b96ca50b1509bd
fallahn <matty_styles@hotmail.com>
2017-06-21 15:53:45 +0100

    fix correctly reporting last GID in a tileset closes #13

--- tmxlite/include/tmxlite/Tileset.hpp
+++ tmxlite/include/tmxlite/Tileset.hpp
@@ -113,7 +113,7 @@ namespace tmx
         \brief Returns the last GID of this tile set.
         This is the ID of the last tile in the tile set.
         */
-        std::uint32_t getLastGID() const { return m_firstGID + getTileCount(); }
+        std::uint32_t getLastGID() const { return m_firstGID + getTileCount() - 1; }
         /*!
         \brief Returns the name of this tile set.
         */
